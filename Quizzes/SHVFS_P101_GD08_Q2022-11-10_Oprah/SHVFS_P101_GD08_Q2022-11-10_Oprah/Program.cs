25eb17a8d8e5dda6750b7fcd64bb74c818e43197
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2022-11-10 20:25:55 +0800

    111

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Oprah/SHVFS_P101_GD08_Q2022-11-10_Oprah/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace QUIZ
+{
+
+    class QUIZ
+    {
+        public static void Main()
+        {
+            int xuanzeshu = 1, lingshigong;
+            Console.WriteLine("welcome! Please input a word to GUESS Please!");
+            xuanzeshu = Convert.ToInt32(Console.ReadLine());
+
+            Random r = new Random();
+            int x = r.Next(100);
+            lingshigong = 0;
+            {
+                while (lingshigong != x)
+                {
+                    lingshigong = Convert.ToInt32(Console.ReadLine());
+                    if (lingshigong < x)
+                    {
+                        Console.WriteLine("NO change and try again.");
+                    }
+
+                }
+            }
+            Console.WriteLine("YES! you are right!! the word is 'word'");
+        }
+    }
+}
+    
+

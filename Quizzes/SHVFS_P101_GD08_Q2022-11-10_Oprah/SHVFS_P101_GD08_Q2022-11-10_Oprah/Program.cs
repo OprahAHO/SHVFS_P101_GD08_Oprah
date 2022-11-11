d0a9c71c632ce71c7a4571e817e400235a0b5d0a
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2022-11-11 17:20:27 +0800

    1

--- Quizzes/SHVFS_P101_GD08_Q2022-11-10_Oprah/SHVFS_P101_GD08_Q2022-11-10_Oprah/Program.cs
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Oprah/SHVFS_P101_GD08_Q2022-11-10_Oprah/Program.cs
@@ -6,26 +6,38 @@ namespace QUIZ
     {
         public static void Main()
         {
-            int xuanzeshu = 1, lingshigong;
-            Console.WriteLine("welcome! Please input a word to GUESS Please!");
-            xuanzeshu = Convert.ToInt32(Console.ReadLine());
+            int i;
+            string name;
+            string[]applename = {"Oprah","Naomi","Cloud","Johnny" };
+            Random random = new Random();
+            int randomnumber=random.Next(0,3);
 
-            Random r = new Random();
-            int x = r.Next(100);
-            lingshigong = 0;
+
+            Console.WriteLine("Welcome to GUESS GAME!! Please typ U name!!!");
+            string username = Console.ReadLine();
+            Console.Clear();
+
+           
+            for (i=1; ; i++)
             {
-                while (lingshigong != x)
-                {
-                    lingshigong = Convert.ToInt32(Console.ReadLine());
-                    if (lingshigong < x)
-                    {
-                        Console.WriteLine("NO change and try again.");
-                    }
+                Console.WriteLine($"Hi~ {username}. We have 4 apples. Oprah, Naomi, Cloud, Johnny. And just ONE is best. SO GUESS which ONE");
+                name = Console.ReadLine();
 
+                if (name != applename[randomnumber])
+                {
+                   
+                    Console.WriteLine("You are wrong! Try again!");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
+                if (name == applename[randomnumber]) break;
             }
-            Console.WriteLine("YES! you are right!! the word is 'word'");
+            Console.WriteLine($"You are right! And you Victory after {i} attempts！ ");
+            Console.WriteLine("Bye!!");
+
         }
+        
+
     }
 }
     

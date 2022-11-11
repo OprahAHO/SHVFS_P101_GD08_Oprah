c92040708f070e72b64e54d364470c5c8a86c08c
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2022-11-11 20:10:14 +0800

    1

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-11_Oprah/SHVFS_P101_GD08_Q2022-11-11_Oprah/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace P101Q_Quiz2
+{
+    public class Snake
+    {
+       public static void Main(string[] args)
+        {
+           
+        }
+        public class GAME
+        {
+            public int X;
+            public int Y;
+            public int Z;
+            public GAME() { }
+            public GAME(int x, int y, int z) 
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+        }        
+    }
+}
+ 
+    

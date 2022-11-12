e0424e9f47b73a0d56fc38efdcef9a47f052a7be
OprahAHO <115708026+OprahAHO@users.noreply.github.com>
2022-11-12 18:15:59 +0800

    1

--- Quizzes/SHVFS_P101_GD08_Q2022-11-11_Oprah/SHVFS_P101_GD08_Q2022-11-11_Oprah/Program.cs
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-11_Oprah/SHVFS_P101_GD08_Q2022-11-11_Oprah/Program.cs
@@ -1,28 +1,66 @@
 ﻿using System;
-using System.Xml.Schema;
-
-namespace P101Q_Quiz2
+namespace SHVFS_P101_GD08_Oprah
 {
-    public class Snake
+    public class program
     {
-       public static void Main(string[] args)
+        public static void Main()
         {
-           
+
+            Game[] sunnyhomeland = new Game[5];
+            sunnyhomeland[0] = new Game("Cloud", 1, 1, 1);
+            sunnyhomeland[1] = new Game("Johnny", 2, 2, 2);
+            sunnyhomeland[2] = new Game("Bandy", 3, 3, 3);
+            sunnyhomeland[3] = new Game("Naomi", 1, 1, 1);
+            sunnyhomeland[4] = new Game("Oprah", 4, 4, 4);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j) continue;
+                    if (sunnyhomeland[i].compare(sunnyhomeland[j]))
+                    {
+                        Console.WriteLine($"No.{i} and No.{j} have the same position of [{sunnyhomeland[i].pos.X},{sunnyhomeland[i].pos.X},{sunnyhomeland[i].pos.X}]");
+                    }
+                }
+            }
+            Console.ReadLine();
         }
-        public class GAME
+
+
+        public struct Position
         {
             public int X;
             public int Y;
             public int Z;
-            public GAME() { }
-            public GAME(int x, int y, int z) 
+
+            public Position(int x, int y, int z)
             {
                 X = x;
                 Y = y;
                 Z = z;
             }
-        }        
+
+        }
+        public class Game
+        {
+            public string Name;
+            public Position pos;
+
+            public Game(string name, int x, int y, int z)
+            {
+                Name = name;
+                pos.X = x;
+                pos.Y = y;
+                pos.Z = z;
+            }
+
+            public bool compare(Game game)
+            {
+                return game.pos.X == pos.X && game.pos.Y == pos.Y && game.pos.Z == pos.Z;
+            }
+        }
+
+
     }
 }
- 
-    
+
